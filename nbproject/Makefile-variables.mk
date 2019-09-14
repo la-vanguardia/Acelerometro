a16a318b93bc91b62e9753a1e655a7f477dc32a6
IsaiasGatica <isaiasgatica1@gmail.com>
2019-09-14 09:28:20 -0300

    Proyecto iniciado

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Acelerometro.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Acelerometro.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=acelerometro.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/acelerometro.tar
