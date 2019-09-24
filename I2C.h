73daf7966e0c89a3273344ca2da73b916c0c4099
EteriumD <Lautaroandressaez@gmail.com>
2019-09-24 16:23:09 -0300

    Se creo la libreria de I2C

--- I2C.h
+++ I2C.h
@@ -1,5 +1,83 @@
-void iniciarComunicacion(unsigned char direccion, unsigned char read); //read=1 r, read=0 w
+#define READ 1
+#define WRITE 0
+
+void iniciarComunicacion(unsigned char codigo_familia, unsigned char read); //read=1 r, read=0 w
 void trasmitirDato(unsigned char dato);
-void trasmitirDatos(unsigned char *datos, unsigned char numero_datos, unsigned char dirreccion);
-unsigned char recibirDato();
-void recibirDatos(unsigned char *datos, unsigned char numero_datos, unsigned char dirreccion);
+void trasmitirDatos(unsigned char *datos, unsigned char numero_datos, unsigned char dirreccion, unsigned char codigo_familia);
+unsigned char recibirDato(unsigned char detener);
+void recibirDatos(unsigned char *datos, unsigned char numero_datos, unsigned char dirreccion, unsigned char codigo_familia);
+void resetearI2C();
+void iniciarI2C();
+void detenerI2C();
+
+void inciarComunicacion(unsigned char codigo_familia, unsigned char read){
+    unsigned char codigo = ( codigo_familia<<1 ) + read;
+    iniciarI2C();
+    trasmitirDato( codigo );
+}
+
+void trasmitirDato(unsigned char dato){
+    IFS1bits.MI2C1IF = 0; //bajo la bandera
+    
+    I2C1TRN = dato; //envio el dato
+    
+    while(I2C1STATbits.ACKSTAT == 1); //espero que baje el ackstat
+    while(IFS1bits.MI2C1IF == 1); //
+    IFS1bits.MI2C1IF = 0;
+    while(I2C1STATbits.TRSTAT == 1); //espero al tr
+}
+
+void trasmirDatos(unsigned char *datos, unsigned char numero_datos, unsigned char dirreccion, unsigned char codigo_familia){
+    iniciarComunicacion(codigo_familia, WRITE);
+    trasmitirDato(dirreccion);
+    iniciarComunicacion(codigo_familia, WRITE);
+    unsigned char i;
+    for(i=0, i<numero_datos; i++){
+        trasmitirDato( datos[i] );
+    }
+    detenerI2C();
+}
+
+unsigned char recibirDato(unsigned char detener){
+    unsigned char dato;
+    
+    IFS1bits.MI2C1IF = 0; //bajo bandera
+    I2C1CONbits.RCEN = 1; //habilito recepcion
+    while(IFS1bits.MI2C1IF == 1); //espero bandera
+    IFS1bits.MI2C1IF = 0; //bajo bandera
+    
+    dato = I2C1RCV; //leo el dato
+    
+    if(detener == 1){
+        detenerI2C();
+    }
+    
+    return dato;
+}
+
+void recibirDatos(unsigned char *datos, unsigned char numero_datos, unsigned char dirreccion, unsigned char codigo_familia){
+    iniciarI2C();
+    iniciarComunicacion(codigo_familia, WRITE);
+    trasmitirDato(dirreccion);
+    iniciarComunicacion(codigo_familia, READ);
+    unsigned char i=0;
+    for(i=0; i<numero_datos; i++){
+        
+        datos[i] = recibirDato(0);
+        __delay_us(20);
+    }
+    
+    detenerI2C();
+}
+
+void iniciarI2C(){
+    I2C1CONbits.SEN = 1;    
+    while(I2C1CONbits.SEN == 1);
+}
+
+
+void detenerI2C(){
+    I2C1CONbits.ACKDT = 1; // ACK
+    I2CCONbits.ACKEN = 1; // habilitador ACK
+    while(I2CCONbits.ACKEN == 1);
+}
